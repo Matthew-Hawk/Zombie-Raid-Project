908744f8985691b8dd89dac9edbdbd5b04075c52
TheHawk4453 Hawk <matthawkins44@gmail.com>
2022-10-30 16:14:48 -0500

    Cleaned up code, added cc, added cursor events, few special animations

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Catagories from "./Components/Catagories/Catagories"
 
 function App() {
 
-
   return (
     <div className="app">
      <h1 className="app__t">Z-Run</h1>
      <h2 className="app__ht">How to play</h2>
      <h3 className="app__hh">Hello, and welcome to Z-Run! the way this works is very simple, you chose 1 option from each column and press the run-it button at the end to see if you made it out alive. Seems easy enough? Then prove it! </h3>
-    <Catagories />
+     <Catagories />
+     <footer className="cc">
+      <a href="https://github.com/Matthew-Hawk" className="cc__link">Created by: Matthew Hawkins</a>
+      <p> © all rights reserved</p>
+     </footer>
     </div>
   );
 }
--- src/Components/Catagories/Catagories.js
+++ src/Components/Catagories/Catagories.js
@@ -3,13 +3,6 @@ import React, { useState } from 'react';
 
 
 function Catagories() {
-
-    // Variables for game
-    // var w = 0
-    // var a = 0
-    // var f = 0
-    // var count = w + a + f
-
     // names for selected itmes
     const [wName, setWName] = useState("")
     const [aName, setAName] = useState("")
@@ -20,53 +13,47 @@ function Catagories() {
     const [armor, setArmor] = useState(null)
     const [food, setFood] = useState(null)
 
-    // state for total and count
-    const [total, setTotal] = useState("");
-    // const [count , setCount] = useState(weapon + armor + food)
+    // state for total
+    const [outcome, setOutcome] = useState("");
 
     // Logic for seeing the players outcome bases on total
     const count = weapon + armor + food
     function check(){
-      // console.log("Count: "+""+count)
-      
       if(count <= 5){
-        setTotal("You're Zombie Food 😖")
+        setOutcome("You're Zombie Food 😖")
       }else if(count <= 7){
-        setTotal("You lived! 🥳")
+        setOutcome("You lived! 🥳")
       }else if(count <= 9){
-        setTotal("You were VERY Greedy, but ya made it 😎")
+        setOutcome("You were VERY Greedy, but ya made it 😎")
       } else{
-        setTotal("☠️ You died ☠️")
+        setOutcome("☠️ You died ☠️")
       }
     }
     //This resets the total and count state for a new game
     function reset (){
-      setTotal("")
+      setOutcome("")
       setWName("")
       setAName("")
       setFName("")
     }
-
-    //onClick functions for the sections
+    //onClick logic for weapons
     function wselect(numToAdd, WeaponName) {
       setWeapon(numToAdd)
       setWName(WeaponName)
       // console.log(w)
     }
-
+    //onClick logic for armor
     function aselect(numToAdd, armorName) {
       setArmor(numToAdd)
       setAName(armorName)
       // console.log(a)
     }
-
+    //onClick logic for food
     function fselect(numToAdd, foodName) {
       setFood(numToAdd)
       setFName(foodName)
       // console.log(f)
-    }
-
-
+    }  
   return (
     <div className='options'>
       <div className="catO">
@@ -75,30 +62,35 @@ function Catagories() {
             <button className='cat__wbutton' onClick={() => wselect(3, "Bat")}>Bat</button>
             <button className='cat__wbutton' onClick={() => wselect(5, "Gun")}>Gun</button>
             <button className='cat__wbutton' onClick={() => wselect(2, "Glue")}>Glue</button>
-            <h2 className="cat__selected">You selected: {wName}</h2>
+            {/* <h2 className="cat__selected">You selected: {wName}</h2> */}
         </section>
         <section className='cat'>
             <h3 className='cat__title bob'>Armor</h3>
             <button className='cat__abutton' onClick={() => aselect(3, "Jacket")}>Jacket</button>
             <button className='cat__abutton' onClick={() => aselect(4, "Leather")}>Leather</button>
             <button className='cat__abutton'onClick={() => aselect(9, "Scarf")}>Scarf</button>
-            <h2 className="cat__selected">You selected: {aName}</h2>
+            {/* <h2 className="cat__selected">You selected: {aName}</h2> */}
         </section>
         <section className='cat'>
             <h3 className='cat__title'>Food</h3>
             <button className='cat__fbutton' onClick={() => fselect(3, "Slurp")}>Slurpy</button>
             <button className='cat__fbutton' onClick={() => fselect(2, "Cherry Soda")}>Cherry Soda</button>
             <button className='cat__fbutton' onClick={() => fselect(9, "Spicy Gum")}>Spicy Gum</button>
-            <h2 className="cat__selected">You selected: {fName}</h2>
-
+            {/* <h2 className="cat__selected">You selected: {fName}</h2> */}
+        </section>
+        <section className="cat">
+            <h3 className="cat__title">Equipment</h3>
+            <h2 className="cat__selected">{wName}</h2>
+            <h2 className="cat__selected">{aName}</h2>
+            <h2 className="cat__selected">{fName}</h2>
         </section>
       </div>
       <div className="button-div">
         <button className="button-div__sub" onClick={check}>Run It</button>
         <button className="button-div__res" onClick={reset}>Reset</button>
       </div>
-        <h1 className="total">{total}</h1>
-        <h3 className="total">{count}</h3>
+        <h1 className="total">{outcome}</h1>
+        {/* <h3 className="total">{count}</h3> */}
     </div>
   )
 }
